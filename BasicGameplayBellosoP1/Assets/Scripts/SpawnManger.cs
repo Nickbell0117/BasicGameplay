70482995b4df93a51e15791202a396ab71cee335
Nickbell0117 <nickbell117@icloud.com>
2023-03-16 09:00:26 -0700

    Finished Basic Game Play
    Actually finished basic game play, fixed all issues.

--- BasicGameplayBellosoP1/Assets/Scripts/SpawnManger.cs
+++ BasicGameplayBellosoP1/Assets/Scripts/SpawnManger.cs
@@ -7,23 +7,29 @@ public class SpawnManger : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-    private float starDelay = 2;
+    private float startDelay = 2;
     private float spawnInterval = 1.5f;
+    private float startDelay2 = 4;
+    private float startDelay3 = 5;
+    private float spawnInterval2 = 3.0f;
+    private float spawnInterval3 = 1.0f;
 
     public float sideSpawnMinZ; 
     public float sideSpawnMaxZ; 
     public float sideSpawnX;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", starDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnLeftAnimal", startDelay2, spawnInterval2);
+        InvokeRepeating("SpawnRightAnimal", startDelay3, spawnInterval3);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     
     void SpawnRandomAnimal()
